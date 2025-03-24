117ed10aec47c0f3b676df78a9b4d9989aca7374
beep boop <yurykgn45@gmail.com>
2025-03-25 01:20:37 +0500

    загрузка/сохранения

--- SaveLoad.cpp
+++ SaveLoad.cpp
@@ -11,58 +11,7 @@
 using namespace std;
 bool autoSaveEnabled = true; // Флаг для включения/выключения автосохранения
 //закомментированные параметры пока неактивны, тк относятся к другой части кода
-struct NPC
-{
-    //mob MOB;			//что за моб(гоблин,скелет и тд
-    //mobClass MOBCLASS;		//тип моба(игрок, житель или враг)
-    char* name = new char[30];      //имя
-    int health;                     //текущее здоровье
-    int endurace;                   //текущая выносливость
-    int mana;                       //мана
-    int strenght;                   //сила
-    int intelligence;               //интеллект
-    int dexterity;                  //ловкость
-    int money;                      //деньги
-    int experience;                 //опыт игрока
-    int experienceFromMonster;      //с монстров
-    int levelPlayer;                //уровень игрока
-    int levelNPC;                   //уровень нпс
-    int speed;                      //скорость персонажа
-    int attackSpeed;                //скорость атаки персонажа
-    int vision;                     //поле зрение
-    int aggression;                 //агрессия
-    //COORD coordinations;            //координаты
-    bool Death;                     //Смерт
-    int* Buffs;			//Баффы по дефолту отсутствуют 
-    int CountBuffs;
 
-};
-struct CONFIG
-{
-    int MapHeight;
-    int MapWidth;
-    int RoomHeight;
-    int RoomWidth;
-    int Wisibility;
-    int Seed;
-    int RoomsNumber;
-    int MenuHeight;
-    int Menuwidth;
-    int AUTO_SAVE_INTERVAL; // Интервал автосохранения в секундах
-};
-struct QUEST
-{
-    int ID;
-    bool is_completed;
-    int step;
-};
-//templ больше не используется
-struct MAP
-{
-    int size;
-    int* MAP_int;
-    char* MAP_char;
-};
 NPC* base_npc;
 //ITEMS base_items; //структур диалогов и предметов пока нет :(
 //DIALOGS base_dialogs;
